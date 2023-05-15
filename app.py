14d12832b84ad15de80b1065675137eaeaac2a3b
JanaKusch <s_kusch21@stud.hwr-berlin.de>
2023-05-15 10:25:13 +0200

    added functioning url references to templates

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    if request.method == "POST":
-        # handle form submission
-        pass
-    else:
-        return render_template("register.html")
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/sign_up')
+def sign_up():
+    return render_template('sign_up.html')
+
+@app.route("/signed_up")
+def signed_up():
+    return render_template("signed_up.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- users.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    email TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL
-);
